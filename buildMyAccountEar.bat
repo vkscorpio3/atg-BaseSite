d3f1de9d1f0adca76bc7f31824da12e39d9a0abc
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2015-08-08 11:41:14 -0500

    First Commit

---
+++ buildMyAccountEar.bat
@@ -0,0 +1,4 @@
+del C:\Oracle\Middleware\Oracle_Home\user_projects\domains\base_domain\servers\SM1\upload\MyAccount.ear
+c:/ATG/ATG11.1/home/bin/runAssembler -unpack D:\Study\Servers\JBoss\wildfly-8.2.0.Final\standalone\deployments\MyAccount.ear -m MyDomain.MyAccount
+
+java:jboss/datasources/jBossATGOracle
